5f4dcd9032cb3a6ac24fe846f5ad10d1c332c4df
Admin <spirit2914@outlook.com>
2020-07-11 19:57:18 -0500

    Initial commit 2

---
+++ HTML-CSS-Projects-Updated-Course/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("This website uses cookies");
+function My_First_Function()
+			{
+		        var String = "Kiss me, I'm Irish!";
+				var result = String.fontcolor("green");
+				document.getElementById("Irish").innerHTML = result;
+			}
